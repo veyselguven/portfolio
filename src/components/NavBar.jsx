4d200b9b76e18ac5cb024678797dfc901f575898
veysel basbaydar <vgbasbaydar@gmail.com>
2022-04-21 11:31:10 -0700

    home component is done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -52,7 +52,7 @@ const NavBar = () => {
       </ul>
 
       {/* Social Icons */}
-      <div className="flex fixed flex-col top-[35%] left-0">
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500">
             <a
@@ -81,7 +81,7 @@ const NavBar = () => {
               <HiOutlineMail size={30} />
             </a>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500">
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
               href="/"
