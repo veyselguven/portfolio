f00fbdc6df35879125c03c63fa94d3ebd3db763a
veysel basbaydar <vgbasbaydar@gmail.com>
2022-04-21 08:53:12 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -21,5 +21,5 @@ function App() {
     </div>
   );
 }
-
+//test
 export default App;
